f9533ad04e96f32bee0d69b6fd7f7bfdf28609e6
Mem0Real <juniourmimo@gmail.com>
2023-06-20 01:14:55 +0300

    Search modal works now.
    I am currently debating weather I should change the db entry to include all it's predecessors' ids inorder to use them as link paths or if i should fetch them from the nearest predecessor and so on....Decisions, decisions.

--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -10,6 +10,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 import getData from "./getData";
+import Link from "next/link";
 
 const style = {
   // position: "absolute",
@@ -25,12 +26,9 @@ const style = {
 
 export default function Search({ modal, closeSearch }) {
   const [searchQuery, setSearchQuery] = useState("");
-  const [filteredCategoryData, setFilteredCategoryData] = useState();
-  const [filteredParentData, setFilteredParentData] = useState();
-  const [filteredChildData, setFilteredChildrenData] = useState();
-  const [filteredItemData, setFilteredItemData] = useState();
 
-  let content;
+  const [content, setContent] = useState();
+  let stat;
   const { data, error, isLoading } = getData();
 
   const isObjEmpty = (obj) => {
@@ -41,76 +39,96 @@ export default function Search({ modal, closeSearch }) {
     const searchWord = e.target.value.toLowerCase();
     setSearchQuery(searchWord);
 
-    if (error) content = <div>Failed to load</div>;
-    else if (isLoading) content = <div>loading...</div>;
-    else {
+    if (error) {
+      setContent("Failed to load");
+    } else if (isLoading) {
+      setContent("loading...");
+    }
+    if (data) {
       const firstArray = data[0].filter((val) => {
-        let query = val.name.toLowerCase();
+        let name = val.name.toLowerCase();
+        let description = val.description?.toLowerCase();
 
-        return query.includes(searchWord);
+        return (name || description).includes(searchWord);
       });
-      // setFilteredCategoryData(firstArray);
 
       const secondArray = data[1].filter((val) => {
-        let query = val.name.toLowerCase();
+        let name = val.name.toLowerCase();
+        let description = val.description?.toLowerCase();
 
-        return query.includes(searchWord);
+        return (name || description).includes(searchWord);
       });
-      // setFilteredParentData(secondArray);
 
       const thirdArray = data[2].filter((val) => {
-        let query = val.name.toLowerCase();
+        let name = val.name.toLowerCase();
+        let description = val.description?.toLowerCase();
 
-        return query.includes(searchWord);
+        return (name || description).includes(searchWord);
       });
-      // setFilteredChildrenData(thirdArray);
 
       const fourthArray = data[3].filter((val) => {
-        let query = val.name.toLowerCase();
+        let name = val.name.toLowerCase();
+        let brand = val.brand?.toLowerCase();
+        let model = val.model?.toLowerCase();
+        let description = val.description?.toLowerCase();
 
-        return query.includes(searchWord);
+        return (name || brand || model || description).includes(searchWord);
       });
 
-      // setFilteredItemData(fourthArray);
+      if (
+        !isObjEmpty(firstArray) ||
+        !isObjEmpty(secondArray) ||
+        !isObjEmpty(thirdArray) ||
+        !isObjEmpty(fourthArray)
+      )
+        stat = true;
+      else stat = false;
 
-      content = (
+      console.log(stat);
+      setContent(
         <div className="flex flex-col gap-6">
-          {!isObjEmpty(firstArray) && (
+          {firstArray && !isObjEmpty(firstArray) && (
             <div className="flex flex-col itmes-start gap-4 border-b border-neutral-200">
               <h1 className="text-start md:ms-3 text-lg font-semibold underline w-full">
                 Categories
               </h1>
-              {firstArray.map((category) => {
-                return <p key={category.id}>{category.name}</p>;
+              {firstArray.map((category, index) => {
+                return (
+                  <ul className="list-disc">
+                    <li key={category.id} className="list-disc text-black">
+                      {category.name}
+                    </li>
+                  </ul>
+                );
               })}
             </div>
           )}
-          {!isObjEmpty(secondArray) && (
+          {secondArray && !isObjEmpty(secondArray) && (
             <div className="flex flex-col itmes-start gap-4 border-b border-neutral-200">
               <h1 className="text-start md:ms-3 text-lg font-semibold underline w-full">
                 Parents
               </h1>
-              {secondArray.map((parent) => {
+              {secondArray.map((parent, index) => {
                 return <p key={parent.id}>{parent.name}</p>;
               })}
             </div>
           )}
-          {!isObjEmpty(thirdArray) && (
+          {thirdArray && !isObjEmpty(thirdArray) && (
             <div className="flex flex-col itmes-start gap-4 border-b border-neutral-200">
               <h1 className="text-start md:ms-3 text-lg font-semibold underline w-full">
                 Children
               </h1>
-              {thirdArray.map((child) => {
+              {thirdArray.map((child, index) => {
                 return <p key={child.id}>{child.name}</p>;
               })}
             </div>
           )}
-          {!isObjEmpty(fourthArray) && (
+          {fourthArray && !isObjEmpty(fourthArray) && (
             <div className="flex flex-col itmes-start gap-4 border-b border-neutral-200">
               <h1 className="text-start md:ms-3 text-lg font-semibold underline w-full">
                 Products
               </h1>
-              {fourthArray.map((item) => {
+              {fourthArray.map((item, index) => {
                 return <p key={item.id}>{item.name}</p>;
               })}
             </div>
@@ -142,7 +160,9 @@ export default function Search({ modal, closeSearch }) {
       <Fade in={modal}>
         <Box
           sx={style}
-          className="bg-neutral-300 text-neutral-800 absolute top-1/3 left-1/2 w-2/5 border-2 border-none rounded-lg shadow-2xl shadow-black"
+          className={`bg-neutral-300 text-neutral-800 absolute ${
+            searchQuery ? "top-[40%]" : "top-1/4"
+          } left-1/2 w-2/5 max-h-96 overflow-y-scroll no-scrollbar border-2 border-none rounded-lg shadow-2xl shadow-black`}
         >
           <div className="flex flex-col gap-4">
             <div className="flex items-center">
@@ -152,13 +172,18 @@ export default function Search({ modal, closeSearch }) {
               <div className="w-full border-b border-neutral-600/60">
                 <input
                   type="text"
+                  value={searchQuery || ""}
                   className="px-2 md:pl-4 md:pr-16 py-3 w-full rounded-md sm:py-2 flex-1 text-neutral-900 text-lg bg-transparent focus:border-none focus:outline-none"
                   placeholder="Search products..."
                   onChange={handleChange}
                 />
               </div>
             </div>
-            <div className="mt-6 bg-neutral-500">{content}</div>
+            {searchQuery && (
+              <div className="mt-6 h-56 overflow-y-scroll no-scrollbar text-center">
+                {content}
+              </div>
+            )}
           </div>
         </Box>
       </Fade>
