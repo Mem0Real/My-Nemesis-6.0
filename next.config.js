28bf5178ae88b4be6357958cef85cbf90ed09c13
Mem0Real <juniourmimo@gmail.com>
2023-06-27 12:34:06 +0300

    Added analyze bundle

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
+const shouldAnalyzeBundles = process.env.ANALYZE === true;
+
 const nextConfig = {
   experimental: {
     serverActions: true,
   },
-  modularizeImports: {
-    "@mui/material/?(((\\w*)?/?)*)": {
-      transform: "@mui/material/{{ matches.[1] }}/{{member}}",
-    },
-    "@mui/icons-material/?(((\\w*)?/?)*)": {
-      transform: "@mui/icons-material/{{ matches.[1] }}/{{member}}",
-    },
-  },
 };
+
+if (shouldAnalyzeBundles) {
+  const withNextBundleAnalyzer =
+    require("@next/bundle-analyzer")(/* options come there */);
+  nextConfig = withNextBundleAnalyzer(nextConfig);
+}
 
 module.exports = nextConfig;
