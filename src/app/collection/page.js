373d69778b8f00b77842eb1c916981700f8bf912
Mem0Real <juniourmimo@gmail.com>
2023-07-01 16:05:13 +0300

    Forgot collection page casing for categories
    casing test2

--- src/app/collection/page.js
+++ src/app/collection/page.js
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 
-import Categories from "./Categories";
+import Categories from "./categories";
 export default function CollectionPage() {
   return (
     <div className="flex flex-col items-center gap-20 text-neutral-800 mt-6">
