a848874f21998fb4b37c8b11778632eb0a973eda
Mem0Real <juniourmimo@gmail.com>
2023-07-11 09:51:40 +0300

    using jwt along previous iterations
    Lets see if this works in production

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,8 +1,20 @@
 import prisma from "./prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
 import { compare } from "bcryptjs";
 import CredentialsProvider from "next-auth/providers/credentials";
 
+if (!process.env.NEXTAUTH_SECRET) {
+  throw new Error(
+    "please provide process.env.NEXTAUTH_SECRET environment variable"
+  );
+}
+
 export const authOptions = {
+  // adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     CredentialsProvider({
       name: "Credentials",
@@ -25,15 +37,21 @@ export const authOptions = {
           where: { email: credentials.email },
         });
 
-        if (!user || !(await compare(credentials.password, user.password))) {
+        if (!user) {
           throw new Error("User does not exist.");
         }
 
+        const isPasswordValid = await compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!isPasswordValid) throw new Error("Wrong password");
+
         return {
-          id: user.id,
+          id: user.id + "",
           email: user.email,
           name: user.name,
-          randomKey: "Nemesis Admin",
         };
       },
     }),
