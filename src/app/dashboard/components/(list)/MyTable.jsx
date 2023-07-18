8b45dfedcf0ff7c4bb9e79891c414c447ac8a84f
Mem0Real <juniourmimo@gmail.com>
2023-07-18 18:46:31 +0300

    Going to see if its responsive in production

--- src/app/dashboard/components/(list)/MyTable.jsx
+++ src/app/dashboard/components/(list)/MyTable.jsx
@@ -119,8 +119,8 @@ export default function MyTable() {
       }}
     >
       <div className="table-container">
-        <div className="mx-auto w-[95%]">
-          <table className="table-fixed w-full overflow-auto text-sm">
+        <div className="mx-auto w-[95%] overflow-auto">
+          <table className="table-fixed w-full text-sm">
             <thead className="border-b border-black">
               <tr className="">
                 <th className="text-center md:text-start py-5 w-56 md:w-72 lg:w-96">
