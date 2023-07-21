f953216aef650c11d7409e042d46f08a9dc3cd5f
Mem0Real <juniourmimo@gmail.com>
2023-07-21 06:48:32 +0300

    Added custom modal for info modal finishing the whole cart modals
    Gonna do the same for search

--- src/app/cart/CartCustom.jsx
+++ src/app/cart/CartCustom.jsx
@@ -1,15 +1,23 @@
 "use client";
 import React, { useState, useEffect, useRef } from "react";
-import ContactInfo from "./ContactInfo";
-import { Add, Remove, Clear } from "@mui/icons-material";
+import dynamic from "next/dynamic";
+
+// import ContactInfo from "./ContactInfo";
+// const ContactInfoCustom = dynamic(() => import("./ContactInfoCustom"));
+
 import { useProductContext } from "@/context/productContext";
 import { toast } from "react-hot-toast";
 
 import { setCookie, parseCookies } from "nookies";
+
+import { PlusOutlined, MinusOutlined, DeleteOutlined } from "@ant-design/icons";
+import { motion, AnimatePresence } from "framer-motion";
+import { useCartContext } from "../components/CartBase";
+
 export default function CartCustom({ closeCartModal }) {
-  const [data, setData] = useState(false);
-  const [infoModal, showInfoModal] = useState(false);
-  const [cartList, setCartList] = useState();
+  // const [data, setData] = useState(false);
+  // const [infoModal, showInfoModal] = useState(false);
+  // const [cartList, setCartList] = useState();
 
   const {
     subtractQuantity,
@@ -20,8 +28,19 @@ export default function CartCustom({ closeCartModal }) {
     setUpdater,
   } = useProductContext();
 
+  const {
+    handleOrder,
+    cartList,
+    clearCart,
+    TAX_RATE,
+    ccyFormat,
+    invoiceSubtotal,
+    invoiceTaxes,
+    invoiceTotal,
+  } = useCartContext();
+
   const modalRef = useRef();
-  const cookieStore = parseCookies();
+  // const cookieStore = parseCookies();
 
   useEffect(() => {
     let handler = (e) => {
@@ -29,24 +48,23 @@ export default function CartCustom({ closeCartModal }) {
         closeCartModal();
       }
     };
-
     document.addEventListener("mousedown", handler);
 
     return () => document.removeEventListener("mousedown", handler);
   }, []);
 
-  useEffect(() => {
-    let cart;
-    if (cookieStore.Cart && cookieStore?.Cart !== undefined)
-      cart = JSON.parse(cookieStore.Cart);
+  // useEffect(() => {
+  //   let cart;
+  //   if (cookieStore.Cart && cookieStore?.Cart !== undefined)
+  //     cart = JSON.parse(cookieStore.Cart);
 
-    if (cart?.length > 0) {
-      setData(() => true);
-      setCartList(() => cart);
-    } else {
-      setData(() => false);
-    }
-  }, [updater]);
+  //   if (cart?.length > 0) {
+  //     setData(() => true);
+  //     setCartList(() => cart);
+  //   } else {
+  //     setData(() => false);
+  //   }
+  // }, [updater]);
 
   const handleMinus = (id) => {
     subtractQuantity(id);
@@ -68,229 +86,218 @@ export default function CartCustom({ closeCartModal }) {
     setUpdater((prev) => !prev);
   };
 
-  const clearCart = () => {
-    setCookie(null, "Cart", JSON.stringify([]));
-    setCookie(null, "Product", JSON.stringify([]));
-    setCookie(null, "Cart_State", false);
-
-    // setUpdater((prev) => !prev);
-    setCartList(() => []);
-    setData(() => false);
-
-    toast("Cart cleared!");
-    setTimeout(() => {
-      closeCartModal();
-    }, 100);
-  };
-
-  const handleOrder = () => {
-    closeCartModal();
-    showInfoModal(() => true);
-  };
-
-  const closeInfoModal = () => {
-    showInfoModal(() => false);
-  };
-
-  const TAX_RATE = 0.07;
-
-  function ccyFormat(num) {
-    return `${num.toFixed(2)}`;
-  }
-
-  function subtotal() {
-    if (cartList?.length > 0) {
-      let allPrice = cartList.map((items) => {
-        return parseFloat(items.totalPrice);
-      });
-      allPrice = allPrice.reduce((sum, i) => sum + i, 0);
-      return allPrice;
-    }
-  }
-
-  const invoiceSubtotal = subtotal();
-  const invoiceTaxes = TAX_RATE * invoiceSubtotal;
-  const invoiceTotal = invoiceTaxes + invoiceSubtotal;
-
   return (
-    <section
-      className="h-fit my-auto w-[90%] sm:w-[75%] md:w-[60%] lg:w-[60%] mx-auto overflow-y-scroll no-scrollbar rounded-lg bg-neutral-900"
-      ref={modalRef}
-    >
-      <header className="pb-4 relative">
-        <button
-          name="close-cart-modal"
-          type="button"
-          className="absolute top-2 right-5 text-white bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
-          data-modal-hide="authentication-modal"
-          onClick={() => closeCartModal()}
-        >
-          <svg
-            aria-hidden="true"
-            className="w-5 h-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+    <>
+      <section
+        className="h-fit my-auto w-[90%] sm:w-[75%] md:w-[60%] lg:w-[60%] mx-auto overflow-y-scroll no-scrollbar rounded-lg bg-neutral-900"
+        ref={modalRef}
+      >
+        <header className="pb-4 relative">
+          <button
+            name="close-cart-modal"
+            type="button"
+            className="absolute top-2 right-5 text-white bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
+            data-modal-hide="authentication-modal"
+            onClick={() => closeCartModal()}
           >
-            <path
-              fillRule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          <span className="sr-only">Close modal</span>
-        </button>
-        <h3 className="mb-4 py-5 border-b border-neutral-200 text-xl text-center font-medium text-neutral-200">
-          Cart
-        </h3>
-        <h1 className="py-1 ps-5 self-start text-lg px-2 underline underline-offset-2">
-          List of Items
-        </h1>
-      </header>
-      <main className="pb-5">
-        <table className="shadow-inner w-auto md:w-[95%] mx-auto bg-neutral-900 shadow-black/60 rounded rounded-b-2xl border-collapse">
-          <thead>
-            <tr className="border-b border-1 border-neutral-200">
-              <th className="text-left py-4 ps-2">Product</th>
-              <th className="text-center py-4 ps-2">Quantity</th>
-              <th className="text-right py-4 pe-2" colSpan={2} align="right">
-                Price
-              </th>
-              <th className="py-3"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {data == true ? (
-              <>
-                {cartList.map((item) => (
-                  <tr key={item.id} className="border-b border-neutral-200">
-                    <td className="py-3 ps-2">{item.name}</td>
-                    <td className="text-center py-3 ps-2">
-                      <div className="flex items-center gap-2">
-                        <button
-                          onClick={() => handleMinus(item.id)}
-                          type="button"
-                          disabled={item.amount <= 1}
-                          className="disabled:text-neutral-600 disabled:hover:cursor-not-allowed "
-                        >
-                          <Remove />
-                        </button>
-                        <input
-                          type="number"
-                          name="quantity"
-                          id="quantity"
-                          className="text-center pl-2.5 py-2 rounded-xl bg-neutral-800 text-neutral-200 border border-neutral-200"
-                          value={item.amount || ""}
-                          onChange={(e) => handleChange(item.id, e)}
-                          required
-                          min={1}
-                          max={item.quantity}
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span className="sr-only">Close modal</span>
+          </button>
+          <h3 className="mb-4 py-5 border-b border-neutral-200 text-xl text-center font-medium text-neutral-200">
+            Cart
+          </h3>
+          <h1 className="py-1 ps-5 self-start text-lg px-2 underline underline-offset-2">
+            List of Items
+          </h1>
+        </header>
+        <main className="pb-5">
+          <table className="table-fixed shadow-inner w-[95%] mx-auto bg-neutral-900 shadow-white/60 rounded rounded-b-2xl border-collapse">
+            <thead>
+              <tr className="border-b border-1 border-neutral-200">
+                <th className="text-center py-2 ps-2 w-36 md:w-40 lg:w-36">
+                  Product
+                </th>
+                <th className="text-center py-2 w-24 md:w-24 lg:w-24">
+                  Quantity
+                </th>
+                <th className="text-center py-2  w-24 md:w-24 lg:w-24">
+                  Price
+                </th>
+                <th className="w-5 md:w-4 lg:w-5 text-center"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {cartList?.length > 0 ? (
+                <>
+                  {cartList.map((item) => (
+                    <tr key={item.id} className="border-b border-neutral-200">
+                      <td className="text-center py-3 ps-2">{item.name}</td>
+                      <td className="text-center py-3">
+                        <div className="flex items-center gap-2">
+                          <button
+                            onClick={() => handleMinus(item.id)}
+                            type="button"
+                            disabled={item.amount <= 1}
+                            className="disabled:text-neutral-600 disabled:hover:cursor-not-allowed "
+                          >
+                            <MinusOutlined />
+                          </button>
+                          <input
+                            type="number"
+                            name="quantity"
+                            id="quantity"
+                            className="text-center content-center py-2 rounded-lg bg-neutral-800 text-neutral-200 border border-neutral-200"
+                            value={item.amount || ""}
+                            onChange={(e) => handleChange(item.id, e)}
+                            required
+                            min={1}
+                            max={item.quantity}
+                          />
+                          <button
+                            onClick={() => handlePlus(item.id)}
+                            type="button"
+                            disabled={item.amount === item.quantity}
+                            className="disabled:text-neutral-600 disabled:hover:cursor-not-allowed"
+                          >
+                            <PlusOutlined />
+                          </button>
+                        </div>
+                      </td>
+                      <td className="text-center py-3 pe-2">
+                        {item.totalPrice}
+                        <span className="text-sm px-1 text-neutral-600 italic items-center">
+                          ETB
+                        </span>
+                      </td>
+                      <td className="text-center">
+                        <DeleteOutlined
+                          className="text-red-600"
+                          onClick={() => handleRemove(item.id, item.quantity)}
                         />
-                        <button
-                          onClick={() => handlePlus(item.id)}
-                          type="button"
-                          disabled={item.amount === item.quantity}
-                          className="disabled:text-neutral-600 disabled:hover:cursor-not-allowed"
-                        >
-                          <Add />
-                        </button>
-                      </div>
-                    </td>
-                    <td className="text-right py-3 pe-2" colSpan={2}>
-                      {item.totalPrice}
+                      </td>
+                    </tr>
+                  ))}
+                  <tr>
+                    <td rowSpan={3} />
+                    <td className="text-center py-2">SubTotal</td>
+                    <td className="text-center">
+                      {ccyFormat(invoiceSubtotal)}
                       <span className="text-sm px-1 text-neutral-600 italic items-center">
                         ETB
                       </span>
                     </td>
-                    <td>
-                      <button
-                        onClick={() => handleRemove(item.id, item.quantity)}
-                      >
-                        <Clear color="error" />
-                      </button>
+                  </tr>
+                  <tr>
+                    <td className="text-center">
+                      <div className="flex items-center justify-center gap-2">
+                        <p>Tax</p>
+                        <span className="font-thin italic text-sm">
+                          ({`${(TAX_RATE * 100).toFixed(0)} %`})
+                        </span>
+                      </div>
+                    </td>
+                    <td className="text-center py-2">
+                      {ccyFormat(invoiceTaxes)}
+                      <span className="text-sm px-1 text-neutral-600 italic items-center">
+                        ETB
+                      </span>
                     </td>
                   </tr>
-                ))}
-                <tr>
-                  <td rowSpan={3} />
-                  <td colSpan={2} className="py-2">
-                    SubTotal
-                  </td>
-                  <td align="right">
-                    {ccyFormat(invoiceSubtotal)}
-                    <span className="text-sm px-1 text-neutral-600 italic items-center">
-                      ETB
-                    </span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Tax</td>
-                  <td align="right" className="py-2">{`${(
-                    TAX_RATE * 100
-                  ).toFixed(0)} %`}</td>
-                  <td align="right">
-                    {ccyFormat(invoiceTaxes)}
-                    <span className="text-sm px-1 text-neutral-600 italic items-center">
-                      ETB
-                    </span>
+                  <tr className="border-t border-neutral-200">
+                    <td className="text-center py-4">Total</td>
+                    <td className="text-center py-4">
+                      {ccyFormat(invoiceTotal)}
+                      <span className="text-sm px-1 text-neutral-600 italic items-center">
+                        ETB
+                      </span>
+                    </td>
+                  </tr>
+                </>
+              ) : (
+                <tr rowSpan={4}>
+                  <td colSpan={5} rowSpan={4} className="py-20">
+                    <h1 className="text-center text-lg mx-auto font-semibold">
+                      Cart Empty
+                    </h1>
                   </td>
                 </tr>
-                <tr className="border-t border-neutral-200">
-                  <td colSpan={2} className="py-4">
-                    Total
-                  </td>
-                  <td align="right" className="py-4">
-                    {ccyFormat(invoiceTotal)}
-                    <span className="text-sm px-1 text-neutral-600 italic items-center">
-                      ETB
-                    </span>
-                  </td>
-                </tr>
-              </>
-            ) : (
-              <tr rowSpan={4}>
-                <td colSpan={5} rowSpan={4} className="py-20">
-                  <h1 className="text-center text-lg mx-auto font-semibold">
-                    Cart Empty
-                  </h1>
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </main>
-      <footer className="flex items-center justify-center gap-12 w-full text-base">
-        {data && (
-          <>
-            <motion.button
-              key="contactInfo"
-              whileTap={{
-                scale: 0.9,
-              }}
-              whileHover={{
-                borderRadius: "12px",
-              }}
-              className="px-3 py-2 rounded-lg outline outline-1 outline-green-700 mb-4"
-              onClick={handleOrder}
-            >
-              Order
-            </motion.button>
-            <motion.button
-              key="clearCart"
-              whileTap={{
-                scale: 0.9,
-              }}
-              whileHover={{
-                borderRadius: "12px",
-              }}
-              className="px-3 py-2 rounded-lg outline outline-1 outline-red-600 mb-4"
-              onClick={clearCart}
-            >
-              Clear
-            </motion.button>
-          </>
+              )}
+            </tbody>
+          </table>
+        </main>
+        <footer className="flex items-center justify-center gap-12 w-full text-base">
+          {cartList?.length > 0 && (
+            <>
+              <motion.button
+                key="contactInfo"
+                whileTap={{
+                  scale: 0.9,
+                }}
+                whileHover={{
+                  borderRadius: "12px",
+                }}
+                className="px-4 py-2 rounded-lg outline outline-1 outline-green-700 mb-4"
+                onClick={handleOrder}
+              >
+                Order
+              </motion.button>
+              <motion.button
+                key="clearCart"
+                whileTap={{
+                  scale: 0.9,
+                }}
+                whileHover={{
+                  borderRadius: "12px",
+                }}
+                className="px-4 py-2 rounded-lg outline outline-1 outline-red-600 mb-4"
+                onClick={clearCart}
+              >
+                Clear
+              </motion.button>
+            </>
+          )}
+        </footer>
+      </section>
+      {/* <ContactInfo
+        modal={infoModal}
+        closeInfoModal={closeInfoModal}
+        cartList={cartList}
+        orderTotalPrice={invoiceTotal}
+        clearCart={clearCart}
+      /> */}
+      {/* <AnimatePresence>
+        {infoModal && (
+          <motion.div
+            key="innerAddM"
+            initial={"close"}
+            animate={infoModal ? "open" : "close"}
+            variants={variants}
+            exit={"close"}
+            className={`fixed top-0 bottom-0 right-0 left-0 z-10 bg-black/50 backdrop-blur-sm  flex ${
+              infoModal ? "pointer-events-auto" : "pointer-events-none"
+            }`}
+          >
+            <ContactInfoCustom
+              cartList={cartList}
+              orderTotalPrice={invoiceTotal}
+              clearCart={clearCart}
+              closeInfoModal={closeInfoModal}
+            />
+          </motion.div>
         )}
-      </footer>
-    </section>
+      </AnimatePresence> */}
+    </>
   );
 }
--- src/app/components/CartBase.jsx
+++ src/app/components/CartBase.jsx
@@ -4,25 +4,44 @@ import React, { useState, useEffect, createContext, useContext } from "react";
 import dynamic from "next/dynamic";
 
 const CartModal = dynamic(() => import("../cart/CartCustom"));
+const ContactInfoCustom = dynamic(() => import("../cart/ContactInfoCustom"));
 
 import { useProductContext } from "@/context/productContext";
 import { setCookie, parseCookies } from "nookies";
 
 import { motion, AnimatePresence } from "framer-motion";
+import { toast } from "react-hot-toast";
 
 const CartContext = createContext({});
 
 export default function CartBase({ children }) {
   const [cartModal, showCartModal] = useState(false);
+  const [infoModal, showInfoModal] = useState(false);
+
+  const [cartList, setCartList] = useState();
+
   const { updater, setUpdater } = useProductContext();
 
+  const cookieStore = parseCookies();
+
   useEffect(() => {
-    if (cartModal) {
+    if (cartModal || infoModal) {
       document.body.style.overflow = "hidden";
     } else {
       document.body.style.overflow = "auto";
     }
-  }, [cartModal]);
+  }, [cartModal, infoModal]);
+
+  useEffect(() => {
+    let cart;
+    if (cookieStore.Cart && cookieStore?.Cart !== undefined)
+      cart = JSON.parse(cookieStore.Cart);
+
+    if (cart?.length > 0) {
+      setCartList(() => cart);
+    } else {
+    }
+  }, [updater]);
 
   const openCartModal = () => {
     showCartModal(() => true);
@@ -32,6 +51,50 @@ export default function CartBase({ children }) {
     showCartModal(() => false);
     setUpdater((prev) => !prev);
   };
+
+  const handleOrder = () => {
+    closeCartModal();
+    showInfoModal(() => true);
+  };
+
+  const closeInfoModal = () => {
+    showInfoModal(() => false);
+  };
+
+  const clearCart = () => {
+    setCookie(null, "Cart", JSON.stringify([]));
+    setCookie(null, "Product", JSON.stringify([]));
+    setCookie(null, "Cart_State", false);
+
+    // setUpdater((prev) => !prev);
+    setCartList(() => []);
+    // setData(() => false);
+
+    toast("Cart cleared!");
+    setTimeout(() => {
+      closeCartModal();
+    }, 100);
+  };
+
+  const TAX_RATE = 0.07;
+
+  function ccyFormat(num) {
+    return `${num.toFixed(2)}`;
+  }
+
+  function subtotal() {
+    if (cartList?.length > 0) {
+      let allPrice = cartList.map((items) => {
+        return parseFloat(items.totalPrice);
+      });
+      allPrice = allPrice.reduce((sum, i) => sum + i, 0);
+      return allPrice;
+    }
+  }
+
+  const invoiceSubtotal = subtotal();
+  const invoiceTaxes = TAX_RATE * invoiceSubtotal;
+  const invoiceTotal = invoiceTaxes + invoiceSubtotal;
 
   const variants = {
     open: {
@@ -47,7 +110,21 @@ export default function CartBase({ children }) {
   };
 
   return (
-    <CartContext.Provider value={{ cartModal, openCartModal }}>
+    <CartContext.Provider
+      value={{
+        cartModal,
+        openCartModal,
+        infoModal,
+        handleOrder,
+        cartList,
+        clearCart,
+        TAX_RATE,
+        ccyFormat,
+        invoiceSubtotal,
+        invoiceTaxes,
+        invoiceTotal,
+      }}
+    >
       {children}
       <AnimatePresence className="my-3">
         {cartModal && (
@@ -62,6 +139,25 @@ export default function CartBase({ children }) {
             }`}
           >
             <CartModal closeCartModal={closeCartModal} />
+          </motion.div>
+        )}
+        {infoModal && (
+          <motion.div
+            key="innerAddM"
+            initial={"close"}
+            animate={infoModal ? "open" : "close"}
+            variants={variants}
+            exit={"close"}
+            className={`fixed top-0 bottom-0 right-0 left-0 z-10 bg-black/50 backdrop-blur-sm  flex ${
+              infoModal ? "pointer-events-auto" : "pointer-events-none"
+            }`}
+          >
+            <ContactInfoCustom
+              cartList={cartList}
+              orderTotalPrice={invoiceTotal}
+              clearCart={clearCart}
+              closeInfoModal={closeInfoModal}
+            />
           </motion.div>
         )}
       </AnimatePresence>
