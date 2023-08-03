6120e302ec5305cfb2bab66b1e1ec8345f5c7c6d
Mem0Real <juniourmimo@gmail.com>
2023-08-03 16:49:43 +0300

    product restruct

---
+++ src/app/utils/setPriceToZero.jsx
@@ -0,0 +1,16 @@
+import prisma from "@/lib/prisma";
+
+export default async function setPriceToZero() {
+  const items = await prisma.items.findMany({
+    select: { id: true, price: true },
+  });
+
+  items.map(async ({ id, price }) => {
+    if (!price) {
+      const res = await prisma.items.update({
+        where: { id: id },
+        data: { price: 0 },
+      });
+    }
+  });
+}
