6678de8a6112b4a595c4aa2a378e786bd123f426
Mem0Real <juniourmimo@gmail.com>
2023-08-08 18:13:03 +0300

    Added parallax effect for header in landing page
    looks good

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 
 import { motion } from "framer-motion";
+import MultiLayerParallax from "./MultiLayerParallax";
 
 const Header = () => {
   const introHeaderVariants = {
@@ -34,33 +35,89 @@ const Header = () => {
     },
   };
   return (
-    <main className="relative flex w-full grow items-center justify-center py-12 px-4 md:px-12">
-      <motion.header
-        className="flex flex-col gap-4"
-        initial="hide"
-        whileInView="show"
-        exit="hide"
-        variants={introHeaderVariants}
-      >
-        <h1 className="text-center text-3xl md:text-5xl text-neutral-800 dark:text-neutral-200">
-          My Nemesis
-        </h1>
-      </motion.header>
-      {/* <motion.div
-        className="absolute right-0 -bottom-12 w-full sm:w-4/5 md:-bottom-36 md:w-[950px]"
-        initial="hide"
-        whileInView="show"
-        exit="hide"
-        variants={introPictureVariants}
-      >
-        <Image
-          src="/images/nemesisLogo.jpg"
-          alt="My Nemesis"
-          width={96}
-          height={96}
-          className="absolute right-0 -bottom-12 w-2/7 md:-bottom-36"
-        />
-      </motion.div>*/}
+    // <main className="relative flex w-full grow items-center justify-center py-12 px-4 md:px-12">
+    //   <motion.header
+    //     className="flex flex-col gap-4"
+    //     initial="hide"
+    //     whileInView="show"
+    //     exit="hide"
+    //     variants={introHeaderVariants}
+    //   >
+    //     <h1 className="text-center text-3xl md:text-5xl text-neutral-800 dark:text-neutral-200">
+    //       My Nemesis
+    //     </h1>
+    //   </motion.header>
+    // </main>
+    <main>
+      <MultiLayerParallax />
+      <div className="w-full bg-neutral-300 dark:bg-neutral-800">
+        <div className="flex flex-col gap-12 items-center justify-center">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Scelerisque varius morbi enim nunc faucibus a pellentesque sit amet.
+            Mauris vitae ultricies leo integer malesuada nunc vel risus. Tellus
+            in hac habitasse platea dictumst vestibulum rhoncus est. Dignissim
+            convallis aenean et tortor at risus viverra adipiscing.{" "}
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Scelerisque varius morbi enim nunc faucibus a pellentesque sit amet.
+            Mauris vitae ultricies leo integer malesuada nunc vel risus. Tellus
+            in hac habitasse platea dictumst vestibulum rhoncus est. Dignissim
+            convallis aenean et tortor at risus viverra adipiscing.{" "}
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Scelerisque varius morbi enim nunc faucibus a pellentesque sit amet.
+            Mauris vitae ultricies leo integer malesuada nunc vel risus. Tellus
+            in hac habitasse platea dictumst vestibulum rhoncus est. Dignissim
+            convallis aenean et tortor at risus viverra adipiscing.{" "}
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Scelerisque varius morbi enim nunc faucibus a pellentesque sit amet.
+            Mauris vitae ultricies leo integer malesuada nunc vel risus. Tellus
+            in hac habitasse platea dictumst vestibulum rhoncus est. Dignissim
+            convallis aenean et tortor at risus viverra adipiscing.{" "}
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Scelerisque varius morbi enim nunc faucibus a pellentesque sit amet.
+            Mauris vitae ultricies leo integer malesuada nunc vel risus. Tellus
+            in hac habitasse platea dictumst vestibulum rhoncus est. Dignissim
+            convallis aenean et tortor at risus viverra adipiscing.{" "}
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Scelerisque varius morbi enim nunc faucibus a pellentesque sit amet.
+            Mauris vitae ultricies leo integer malesuada nunc vel risus. Tellus
+            in hac habitasse platea dictumst vestibulum rhoncus est. Dignissim
+            convallis aenean et tortor at risus viverra adipiscing.{" "}
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Scelerisque varius morbi enim nunc faucibus a pellentesque sit amet.
+            Mauris vitae ultricies leo integer malesuada nunc vel risus. Tellus
+            in hac habitasse platea dictumst vestibulum rhoncus est. Dignissim
+            convallis aenean et tortor at risus viverra adipiscing.{" "}
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Scelerisque varius morbi enim nunc faucibus a pellentesque sit amet.
+            Mauris vitae ultricies leo integer malesuada nunc vel risus. Tellus
+            in hac habitasse platea dictumst vestibulum rhoncus est. Dignissim
+            convallis aenean et tortor at risus viverra adipiscing.{" "}
+          </p>
+        </div>
+      </div>
     </main>
   );
 };
---
+++ src/app/components/MultiLayerParallax.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { motion, useScroll, useTransform } from "framer-motion";
+import React, { useRef } from "react";
+import { useTheme } from "next-themes";
+
+export default function MultiLayerParallax() {
+  const { theme } = useTheme();
+
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+
+  return (
+    <div className="w-full h-screen overflow-hidden relative grid place-items-center">
+      <motion.h1
+        className="font-bold text-black dark:text-white text-7xl md:text-8xl relative z-10"
+        style={{ y: textY }}
+      >
+        My Nemesis
+      </motion.h1>
+
+      {theme === "light" ? (
+        <motion.div
+          className="absolute inset-0 z-0"
+          style={{
+            backgroundImage: `url(/images/Day.png)`,
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+            y: backgroundY,
+          }}
+        ></motion.div>
+      ) : (
+        <motion.div
+          className="absolute inset-0 z-0"
+          style={{
+            backgroundImage: `url(/images/Night.png)`,
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+            y: backgroundY,
+          }}
+        ></motion.div>
+      )}
+
+      <div
+        className="absolute inset-0 z-20"
+        style={{
+          backgroundImage: `url(/images/Building.png)`,
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+        }}
+      ></div>
+    </div>
+  );
+}
