fbf3d60d0a41b18df1159ec36c6041af6aad2161
Mem0Real <juniourmimo@gmail.com>
2023-08-08 15:52:57 +0300

    cursor mask completed
    starting parallax home page

--- src/app/products/components/CustomCursor.js
+++ src/app/products/components/CustomCursor.js
@@ -5,46 +5,12 @@ import { motion, useScroll } from "framer-motion";
 import { useIcons } from "@/app/utils/CustomIcons";
 
 export default function CustomCursor({ children }) {
-  //if user is hovering an icon, cursor locks at it
-
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
   const [[rotateX, rotateY, scaleX, scaleY], setMovementAnimation] = useState([
     0, 0, 1, 1,
   ]);
 
   const containerRef = useRef();
-
-  const { BlackCursor } = useIcons();
-
-  // const onMouseMove = (e) => {
-  //   const { left, top } = containerRef.current.getBoundingClientRect();
-  //   const newX = e.clientX - left;
-  //   const newY = e.clientY - top;
-  //   const absDeltaX = Math.abs(mousePosition.x - newX);
-  //   const absDeltaY = Math.abs(mousePosition.y - newY);
-  //   setMovementAnimation([
-  //     absDeltaX * 0.5,
-  //     absDeltaY * 0.5,
-  //     1 - absDeltaY * 0.005,
-  //     1 - absDeltaX * 0.005,
-  //   ]);
-  //   setMousePosition({ x: newX, y: newY });
-  // };
-
-  // useEffect(() => {
-  //   const handleMouseMove = (e) => {
-  //     console.log(e.clientX, e.clientY);
-  //     setMousePosition({ x: e.clientX, y: e.clientY });
-  //   };
-  //   const watchScroll = () => {
-  //     window.addEventListener("mousemove", handleMouseMove);
-  //   };
-
-  //   watchScroll();
-  //   return () => {
-  //     window.removeEventListener("mousemove", handleMouseMove);
-  //   };
-  // }, []);
 
   const onMouseMove = (e) => {
     const { left, top } = containerRef.current.getBoundingClientRect();
@@ -53,8 +19,8 @@ export default function CustomCursor({ children }) {
     const absDeltaX = Math.abs(mousePosition.x - newX);
     const absDeltaY = Math.abs(mousePosition.y - newY);
     setMovementAnimation([
+      absDeltaY * 2.5,
       absDeltaX * 2.5,
-      absDeltaY * 1,
       1 - absDeltaY * 0.005,
       1 - absDeltaX * 0.005,
     ]);
@@ -67,11 +33,12 @@ export default function CustomCursor({ children }) {
       onMouseMove={onMouseMove}
     >
       {children}
+
       <motion.div
-        className={`${styles.cursor} shadow-inner shadow-red-800 border border-neutral-800 dark:border-neutral-200 opacity-60 w-16 h-16 backdrop-blur-sm z-50`}
+        className={`${styles.cursor} shadow-inner shadow-red-800 border border-neutral-800 dark:border-neutral-200 opacity-60 w-20 h-20 backdrop-blur-sm z-50`}
         animate={{
-          translateX: -29,
-          translateY: -29,
+          translateX: -32,
+          translateY: -32,
           rotateX,
           rotateY,
           scaleX,
@@ -80,7 +47,7 @@ export default function CustomCursor({ children }) {
           top: mousePosition.y,
           transition: {
             duration: 0.1,
-            ease: "linear",
+            ease: "circInOut",
           },
         }}
       />
