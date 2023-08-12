65b16b7af773e41d8c9c50a8223d0ee39691de37
Mem0Real <juniourmimo@gmail.com>
2023-08-12 16:07:16 +0300

    added footer navigation

--- src/app/utils/CustomIcons.jsx
+++ src/app/utils/CustomIcons.jsx
@@ -437,6 +437,62 @@ export default function CustomIcons({ children }) {
       <path d="M20.733 20l1.3 .75"></path>
     </svg>
   );
+
+  const FacebookIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-brand-facebook"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3"></path>
+    </svg>
+  );
+
+  const TelegramIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-brand-telegram"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      strokeWidth="1.3"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4"></path>
+    </svg>
+  );
+
+  const InstagramIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-brand-instagram"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      strokeWidth="1.3"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z"></path>
+      <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+      <path d="M16.5 7.5l0 .01"></path>
+    </svg>
+  );
   return (
     <IconsContext.Provider
       value={{
@@ -465,6 +521,9 @@ export default function CustomIcons({ children }) {
         DiscountIcon,
         HourIcon,
         DeliveryTruckIcon,
+        FacebookIcon,
+        TelegramIcon,
+        InstagramIcon,
       }}
     >
       {children}
