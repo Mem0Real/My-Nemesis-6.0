6825e049c3b996eac2c997f51ccce4c72b261693
Mem0Real <juniourmimo@gmail.com>
2023-08-20 20:01:26 +0300

    Updated next-theme dependency inside carouselHolder file to use custom theme

--- src/app/catalogue/components/CarouselHolder.jsx
+++ src/app/catalogue/components/CarouselHolder.jsx
@@ -1,9 +1,23 @@
 "use client";
+
+import { useState, useEffect } from "react";
+
 import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
 import { MouseEvent } from "react";
-import { useTheme } from "next-themes";
+// import { useTheme } from "next-themes";
+import { useThemeContext } from "@/context/ThemeProvider";
 
 export default function CarouselHolder({ children }) {
+  const { update } = useThemeContext();
+
+  const [currentTheme, setCurrentTheme] = useState("");
+
+  useEffect(() => {
+    const theme = localStorage.getItem("isDarkTheme");
+    if (theme === "false") setCurrentTheme("light");
+    else setCurrentTheme("dark");
+  }, [update]);
+
   let mouseX = useMotionValue(0);
   let mouseY = useMotionValue(0);
 
@@ -14,14 +28,14 @@ export default function CarouselHolder({ children }) {
     mouseY.set(clientY - top);
   }
 
-  const { theme } = useTheme();
+  // const { theme } = useTheme();
 
   return (
     <div
       className="group mx-auto w-[95%] relative rounded-xl border border-white/10 bg-gradient-to-r from-white/40 to-white/80 dark:from-black/40 dark:to-neutral-600/80 shadow-inner shadow-black/40 backdrop-blur-sm"
       onMouseMove={handleMouseMove}
     >
-      {theme === "dark" ? (
+      {currentTheme === "dark" ? (
         <motion.div
           className="pointer-events-none absolute -inset-px rounded-xl opacity-0 transition duration-300 group-hover:opacity-100"
           style={{
