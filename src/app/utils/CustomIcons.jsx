f8937d74738c8d9fb6f81b59c40c330eea84271f
Mem0Real <juniourmimo@gmail.com>
2023-08-27 07:11:29 +0300

    Added the small dropdown for customers
    Now comes the main challenge, gsap. Also, gotta fix the scroll

--- src/app/utils/CustomIcons.jsx
+++ src/app/utils/CustomIcons.jsx
@@ -269,23 +269,6 @@ export default function CustomIcons({ children }) {
     </svg>
   );
 
-  // const CloseIcon = (
-  //   <svg
-  //     xmlns="http://www.w3.org/2000/svg"
-  //     width="18"
-  //     height="18"
-  //     viewBox="0 0 24 24"
-  //     fill="none"
-  //     stroke="currentColor"
-  //     strokeWidth="2"
-  //     strokeLinecap="round"
-  //     strokeLinejoin="round"
-  //   >
-  //     <line x1="18" y1="6" x2="6" y2="18"></line>
-  //     <line x1="6" y1="6" x2="18" y2="18"></line>
-  //   </svg>
-  // );
-
   const BagIcon = (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -561,6 +544,30 @@ export default function CustomIcons({ children }) {
       <polyline points="22,6 12,13 2,6"></polyline>
     </svg>
   );
+
+  const SettingsIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="22"
+      height="22"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="4" y1="21" x2="4" y2="14"></line>
+      <line x1="4" y1="10" x2="4" y2="3"></line>
+      <line x1="12" y1="21" x2="12" y2="12"></line>
+      <line x1="12" y1="8" x2="12" y2="3"></line>
+      <line x1="20" y1="21" x2="20" y2="16"></line>
+      <line x1="20" y1="12" x2="20" y2="3"></line>
+      <line x1="1" y1="14" x2="7" y2="14"></line>
+      <line x1="9" y1="8" x2="15" y2="8"></line>
+      <line x1="17" y1="16" x2="23" y2="16"></line>
+    </svg>
+  );
   return (
     <IconsContext.Provider
       value={{
@@ -595,6 +602,7 @@ export default function CustomIcons({ children }) {
         LocationIcon,
         PhoneIcon,
         MessageIcon,
+        SettingsIcon,
       }}
     >
       {children}
