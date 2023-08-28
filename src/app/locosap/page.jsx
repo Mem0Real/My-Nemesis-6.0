ba1de452cf60bff903e7db747b817bd45730f04b
Mem0Real <juniourmimo@gmail.com>
2023-08-28 04:05:32 +0300

    moving text instead of pin

--- src/app/locosap/page.jsx
+++ src/app/locosap/page.jsx
@@ -1,15 +1,23 @@
 "use client";
 
-import { useEffect, useLayoutEffect, useRef } from "react";
+import { useState, useEffect, useLayoutEffect, useRef } from "react";
+import Image from "next/image";
+
+import { useThemeContext } from "@/context/ThemeProvider";
 
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Image from "next/image";
 
 export default function LocoSap() {
-  const root = useRef();
-  const text = useRef();
-  const section = useRef();
+  const { update } = useThemeContext();
+
+  const [currentTheme, setCurrentTheme] = useState("");
+
+  useEffect(() => {
+    const theme = localStorage.getItem("isDarkTheme");
+    if (theme === "false") setCurrentTheme("light");
+    else setCurrentTheme("dark");
+  }, [update]);
 
   useEffect(() => {
     (async () => {
@@ -18,16 +26,36 @@ export default function LocoSap() {
     })();
   });
 
+  const root = useRef();
+  const text = useRef();
+  const section = useRef();
+
   useLayoutEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
     const ctx = gsap.context(() => {
-      ScrollTrigger.create({
-        trigger: text.current,
-        start: "start +=150px",
-        end: "end -=150px",
-        pin: true,
-        scrub: true,
+      // ScrollTrigger.create({
+      //   trigger: text.current,
+      //   start: "start +=300px",
+      //   end: "bottom",
+      //   pin: true,
+      //   scrub: true,
+      // });
+
+      gsap.set(text.current, {
+        y: -50,
+        scale: 0.7,
+      });
+      gsap.to(text.current, {
+        scale: 1,
+        y: 500,
+        scrollTrigger: {
+          trigger: text.current,
+          start: "top +=250",
+          end: "+=800px",
+          // pin: true,
+          scrub: true,
+        },
       });
     }, root);
 
@@ -36,16 +64,43 @@ export default function LocoSap() {
 
   return (
     <main ref={root} className="m-0 w-screen min-h-screen">
-      <div className="h-screen w-full bg-green-600 pt-24 flex flex-col justify-between items-center">
-        <h1 ref={text} className="text-5xl text-neutral-800 z-10">
-          Nemesis
+      <div className="relative grid place-items-center w-screen h-screen">
+        {currentTheme === "light" ? (
+          <Image
+            src="/images/Day.png"
+            fill
+            sizes="(max-width: 768px) 100vw"
+            alt="catalogue"
+            className="object-cover object-center"
+          />
+        ) : (
+          <Image
+            src="/images/Night.png"
+            fill
+            sizes="(max-width: 768px) 100vw"
+            alt="catalogue"
+            className="object-cover object-center"
+          />
+        )}
+        <h1
+          ref={text}
+          className="z-10 text-6xl text-neutral-800 dark:text-neutral-200"
+        >
+          Ethio Machineries
         </h1>
-        <section
-          ref={section}
-          className="self-end w-screen h-[40vh] bg-neutral-400 z-20"
-        />
+        <div ref={section} className="absolute inset-0 z-10">
+          <Image
+            fill
+            alt="dark"
+            className="object-cover object-bottom"
+            src="/images/Building.png"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw"
+            quality={100}
+            priority
+          />
+        </div>
       </div>
-      <div className="h-screen bg-neutral-500 w-screen"></div>
+      <div className="h-screen bg-neutral-900 w-screen z-30"></div>
     </main>
   );
 }
