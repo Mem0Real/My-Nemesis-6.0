7000ff2d7b018de8593bad0260b03b52526b159a
Mem0Real <juniourmimo@gmail.com>
2023-08-30 09:01:56 +0300

    Email looks great now and loading states corrected

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -11,129 +11,141 @@ import { sendMessage } from "./utils/contactActions";
 import { toast } from "react-hot-toast";
 
 const Contact = () => {
-  const [data, setData] = useState({ fullName: "", email: "", message: "" });
-  const [loading, setLoading] = useState(false);
+	const [data, setData] = useState({ fullName: "", email: "", message: "" });
+	const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
+	const handleChange = (e) => {
+		const name = e.target.name;
+		const value = e.target.value;
 
-    setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
+		setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+	};
 
-  const handleSend = async () => {
-    setLoading(() => true);
-    const toastId = toast.loading("Sending Message...");
+	const handleSend = async (e) => {
+		e.preventDefault();
 
-    const request = await sendMessage(data);
-    setLoading(() => false);
-    toast.remove(toastId);
-    if (request?.error) toast.error(request.error, { duration: 10000 });
-    else {
-      toast.remove(toastId);
-      toast.success(request.success);
-      setData({ fullName: "", email: "", message: "" });
-    }
-  };
-  return (
-    <PageWrapper>
-      <div className="w-full flex flex-col items-center justify-center py-8 md:py-10 lg:py-12 shadow-xl shadow-blue-600/20 dark:shadow-blue-400/10 text-neutral-800 dark:text-neutral-200">
-        <h1 className="text-3xl md:text-4xl lg:text-5xl font-extralight italic">
-          Contact Us
-        </h1>
-      </div>
-      <div className="flex flex-col md:flex-row w-[95%] md:w-[70%] mx-auto box-border py-6">
-        <div className="flex-1 rounded-xl">
-          <div className="relative z-0 w-full h-full flex flex-col items-center justify-center gap-5 py-12 md:py-0">
-            <Image
-              src="/images/contact-page.jpg"
-              fill
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
-              alt="Contact"
-              className="object-cover object-center brightness-[.2]"
-              priority
-            />
+		const toastId = toast.loading("Sending Message...");
 
-            <Icons />
-          </div>
-        </div>
-        <div className="flex-1 h-full bg-neutral-100 dark:bg-neutral-900 text-neutral-800 dark:text-neutral-200 box-content rounded-xl rounded-l-none py-12 md:py-0">
-          <div className="flex flex-col gap-4 items-center justify-center">
-            <h1 className="py-5 text-4xl font-semiBold">Send Us A Message</h1>
-            <form
-              action={handleSend}
-              className="w-[90%] mx-auto flex flex-col justify-center items-center gap-12 mt-6"
-            >
-              <div className="relative z-0 group w-3/4 mx-auto">
-                <input
-                  id="fullName"
-                  name="fullName"
-                  type="text"
-                  className="block py-2.5 px-0 w-full text-sm text-neutral-800 dark:text-neutral-200 bg-transparent border-0 border-b-2 border-neutral-400 appearance-none dark:border-neutral-800 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                  onChange={handleChange}
-                />
-                <label
-                  htmlFor="fullName"
-                  className="peer-focus:font-medium absolute text-sm  text-neutral-600 dark:text-neutral-400 duration-300 transform -translate-y-9 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-9"
-                >
-                  Full Name
-                </label>
-              </div>
-              <div className="relative z-0 group w-3/4 mx-auto">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  className="block py-2.5 px-0 w-full text-sm text-neutral-800 dark:text-neutral-200 bg-transparent border-0 border-b-2 border-neutral-400 appearance-none dark:border-neutral-800 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                  onChange={handleChange}
-                />
-                <label
-                  htmlFor="email"
-                  className="peer-focus:font-medium absolute text-sm  text-neutral-600 dark:text-neutral-400 duration-300 transform -translate-y-9 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-9"
-                >
-                  Email
-                </label>
-              </div>
-              <div className="relative z-0 w-3/4 group">
-                <textarea
-                  cols={8}
-                  rows={8}
-                  id="message"
-                  name="message"
-                  type="text"
-                  className="block py-2.5 px-0 w-full text-sm text-neutral-800 dark:text-neutral-200 bg-transparent border-0 border-b-2 border-neutral-400 appearance-none dark:border-neutral-800 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                  onChange={handleChange}
-                />
-                <label
-                  htmlFor="message"
-                  className="peer-focus:font-medium absolute text-sm  text-neutral-600 dark:text-neutral-400 duration-300 transform -translate-y-9 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-9"
-                >
-                  Message
-                </label>
-              </div>
-              <motion.button
-                disabled={loading}
-                name="submit"
-                type="submit"
-                className="px-4 py-1 rounded-md bg-transparent text-neutral-800 dark:text-neutral-200 outline outline-2 outline-neutral-800 dark:outline-neutral-200"
-                whileHover={{
-                  borderRadius: "10px",
-                }}
-                whileTap={{
-                  scale: 0.95,
-                }}
-              >
-                Send
-              </motion.button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </PageWrapper>
-  );
+		setLoading(() => true);
+
+		const request = await sendMessage(data);
+		setLoading(() => false);
+		toast.remove(toastId);
+		if (request?.error) {
+			toast.remove(toastId);
+			toast.error(request.error, { duration: 5000 });
+		} else {
+			toast.remove(toastId);
+			toast.success(request.success);
+			setData({ fullName: "", email: "", message: "" });
+		}
+	};
+	return (
+		<PageWrapper>
+			<div className="w-full flex flex-col items-center justify-center py-8 md:py-10 lg:py-12 shadow-xl shadow-blue-600/20 dark:shadow-blue-400/10 text-neutral-800 dark:text-neutral-200">
+				<h1 className="text-3xl md:text-4xl lg:text-5xl font-extralight italic">
+					Contact Us
+				</h1>
+			</div>
+			<div className="flex flex-col md:flex-row w-[95%] md:w-[70%] mx-auto box-border py-6">
+				<div className="flex-1 rounded-xl">
+					<div className="relative z-0 w-full h-full flex flex-col items-center justify-center gap-5 py-12 md:py-0">
+						<Image
+							src="/images/contact-page.jpg"
+							fill
+							sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
+							alt="Contact"
+							className="object-cover object-center brightness-[.2]"
+							priority
+						/>
+
+						<Icons />
+					</div>
+				</div>
+				<div className="flex-1 h-full bg-neutral-100 dark:bg-neutral-900 text-neutral-800 dark:text-neutral-200 box-content rounded-xl rounded-l-none py-12 md:py-0">
+					<div className="flex flex-col gap-4 items-center justify-center">
+						<h1 className="py-5 text-4xl font-semiBold">Send Us A Message</h1>
+						<form
+							onSubmit={handleSend}
+							className="w-[90%] mx-auto flex flex-col justify-center items-center gap-12 mt-6"
+						>
+							<div className="relative z-0 group w-3/4 mx-auto">
+								<input
+									id="fullName"
+									name="fullName"
+									type="text"
+									className="block py-2.5 px-0 w-full text-sm text-neutral-800 dark:text-neutral-200 bg-transparent border-0 border-b-2 border-neutral-400 appearance-none dark:border-neutral-800 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+									placeholder=" "
+									onChange={handleChange}
+									value={data.fullName || ""}
+									autoFocus
+								/>
+								<label
+									htmlFor="fullName"
+									className="peer-focus:font-medium absolute text-sm  text-neutral-600 dark:text-neutral-400 duration-300 transform -translate-y-9 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-9"
+								>
+									Full Name
+								</label>
+							</div>
+							<div className="relative z-0 group w-3/4 mx-auto">
+								<input
+									id="email"
+									name="email"
+									type="email"
+									className="block py-2.5 px-0 w-full text-sm text-neutral-800 dark:text-neutral-200 bg-transparent border-0 border-b-2 border-neutral-400 appearance-none dark:border-neutral-800 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+									placeholder=" "
+									onChange={handleChange}
+									value={data.email || ""}
+								/>
+								<label
+									htmlFor="email"
+									className="peer-focus:font-medium absolute text-sm  text-neutral-600 dark:text-neutral-400 duration-300 transform -translate-y-9 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-9"
+								>
+									Email
+								</label>
+							</div>
+							<div className="relative z-0 w-3/4 group">
+								<textarea
+									cols={8}
+									rows={8}
+									id="message"
+									name="message"
+									type="text"
+									className="block py-2.5 px-0 w-full text-sm text-neutral-800 dark:text-neutral-200 bg-transparent border-0 border-b-2 border-neutral-400 appearance-none dark:border-neutral-800 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+									placeholder=" "
+									onChange={handleChange}
+									value={data.message || ""}
+								/>
+								<label
+									htmlFor="message"
+									className="peer-focus:font-medium absolute text-sm  text-neutral-600 dark:text-neutral-400 duration-300 transform -translate-y-9 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-9"
+								>
+									Message
+								</label>
+							</div>
+							<div className="pb-12">
+								<motion.button
+									disabled={loading}
+									name="submit"
+									type="submit"
+									className="px-4 py-1 rounded-md bg-transparent disabled:bg-neutral-500 text-neutral-800 dark:text-neutral-200 outline outline-2 outline-neutral-800 dark:outline-neutral-200"
+									whileHover={{
+										borderRadius: "10px",
+									}}
+									whileTap={{
+										scale: 0.95,
+									}}
+									animate={loading ? { scale: 0.8 } : { scale: 1 }}
+								>
+									Send
+								</motion.button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</PageWrapper>
+	);
 };
 
 export default Contact;
--- src/lib/mailService.js
+++ src/lib/mailService.js
@@ -1,39 +1,75 @@
 import nodemailer from "nodemailer";
+const fs = require("fs");
+import { join } from "path";
+
+const path = process.cwd();
+const htmlPath = "src/lib/mail.html";
+
+const location = join(path, htmlPath);
+const htmlTemplate = fs.readFileSync(location, "utf8");
 
 export async function sendMail(sender, subject, message) {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.NODEMAILER_EMAIL,
-      pass: process.env.NODEMAILER_PW,
-    },
-  });
+	const transporter = nodemailer.createTransport({
+		host: "smtp.gmail.com",
+		port: 465,
+		secure: true,
+		auth: {
+			user: process.env.NODEMAILER_EMAIL,
+			pass: process.env.NODEMAILER_PW,
+		},
+	});
 
-  let firstName = subject.toLowerCase();
-  let array = firstName.split(" ");
-  firstName = array[0];
+	let firstName = subject.toLowerCase();
+	let array = firstName.split(" ");
+	firstName = array[0];
 
-  const name = subject;
-  const email = sender;
+	const name = subject;
+	const email = sender;
 
-  const mailOptions = {
-    from: `${name} <${email}>`,
-    to: process.env.NODEMAILER_EMAIL,
-    subject: subject,
-    text: message + "Sent from: " + email,
-    html: `
-    <p>${name}</p> 
-    <div>${message}</div>
-    <p>Sent from:
-    ${email}</p>`,
-  };
+	let htmlContent = htmlTemplate.replace("{{name}}", name);
+	htmlContent = htmlContent.replace("{{message}}", message);
+	htmlContent = htmlContent.replace("{{email}}", email);
 
-  try {
-    await transporter.sendMail(mailOptions);
-    return { success: "Message Sent" };
-  } catch (error) {
-    return { error: "Message Not Sent!" };
-  }
+	const mailOptions = {
+		from: `${name} <${email}>`,
+		to: process.env.NODEMAILER_EMAIL,
+		subject: subject,
+		// text: message + "Sent from: " + email,
+		// html: `<div style={display: flex, }
+		// <p>${name}</p>
+		// <div>${message}</div>
+		// <p>Sent from:
+		// ${email}</p>`,
+
+		// 	html: `
+		//   <div
+		//     style="
+		//       display: flex;
+		//       flex-direction: column;
+		//       justify-content: center;
+		//       align-items: center;
+		//       gap: 5;
+		//       background-color: #313131;
+		//       color: azure;
+		//       width: 100%;
+		//       height: 100%;
+		//       padding-top: 4em;
+		//     "
+		// >
+		//   <h1 style="font-weight: 500; text-decoration: underline; font-size: 50px">
+		//     Ethio Machineries
+		//   </h1>
+		//   <h3 style="margin-top: 15px">You have recieved an email from ${name}</h3>
+		//   <p style="border: 1px solid #acacac; border-radius: 10px">${message}</p>
+		//         <h2 style="margin-top: 30px; align-self: flex-end;">Please reply to ${name} at ${email}</h2>
+		// </div>`,
+		html: htmlContent,
+	};
+
+	try {
+		await transporter.sendMail(mailOptions);
+		return { success: "Message Sent" };
+	} catch (error) {
+		return { error: "Message Not Sent!" };
+	}
 }
