8e398b80569de0815616387a4a47855344ec1784
Mem0Real <juniourmimo@gmail.com>
2023-08-31 18:42:12 +0300

    Key

--- src/app/catalogue/components/Slider.jsx
+++ src/app/catalogue/components/Slider.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
@@ -58,16 +58,18 @@ export default function Slider() {
 			>
 				{images.map((img, index) => {
 					return (
-						<SwiperSlide>
-							<div key={index} className="relative w-56 h-56 rounded-lg">
-								<Image
-									src={img}
-									alt=""
-									fill
-									className="object-contain object-center"
-								/>
-							</div>
-						</SwiperSlide>
+						<React.Fragment key={index}>
+							<SwiperSlide>
+								<div className="relative w-56 h-56 rounded-lg">
+									<Image
+										src={img}
+										alt=""
+										fill
+										className="object-contain object-center"
+									/>
+								</div>
+							</SwiperSlide>
+						</React.Fragment>
 					);
 				})}
 			</Swiper>
