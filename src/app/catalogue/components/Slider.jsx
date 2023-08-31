861c4835564111c34c01ca9d84f47112230a77c9
Mem0Real <juniourmimo@gmail.com>
2023-08-31 18:39:03 +0300

    Trying swiper js for carousel

---
+++ src/app/catalogue/components/Slider.jsx
@@ -0,0 +1,76 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+const images = [
+	"/images/1.png",
+	"/images/2.png",
+	"/images/3.png",
+	"/images/4.png",
+	"/images/5.png",
+	"/images/6.png",
+	"/images/7.png",
+];
+export default function Slider() {
+	// useEffect(() => {
+	// 	const swiper = new Swiper(".swiperContainer", {});
+	// }, []);
+
+	// return (
+	// 	<div className="swiperContainer">
+	// 		<div className="swiperWrapper">
+	// 			{images.map((img, index) => {
+	// 				return (
+	// 					<div key={index} className="relative w-56 h-56 rounded-lg">
+	// 						<Image
+	// 							src={img}
+	// 							alt=""
+	// 							fill
+	// 							className="object-contain object-center"
+	// 						/>
+	// 					</div>
+	// 				);
+	// 			})}
+	// 		</div>
+	// 	</div>
+	// );
+
+	return (
+		<div className="overflow-hidden w-full">
+			<Swiper
+				modules={[Navigation, Pagination, Scrollbar, A11y]}
+				spaceBetween={50}
+				slidesPerView={3}
+				navigation
+				pagination={{ clickable: true }}
+				scrollbar={{ draggable: true }}
+				onSwiper={(swiper) => console.log(swiper)}
+				onSlideChange={() => console.log("slide change")}
+			>
+				{images.map((img, index) => {
+					return (
+						<SwiperSlide>
+							<div key={index} className="relative w-56 h-56 rounded-lg">
+								<Image
+									src={img}
+									alt=""
+									fill
+									className="object-contain object-center"
+								/>
+							</div>
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
+		</div>
+	);
+}
