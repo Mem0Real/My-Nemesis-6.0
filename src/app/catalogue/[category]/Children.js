2882f32a0ef5c20cf3e1c9edb031f210d20cab44
Mem0Real <juniourmimo@gmail.com>
2023-08-31 22:59:04 +0300

    applied it to catalogue

--- src/app/catalogue/[category]/Children.js
+++ src/app/catalogue/[category]/Children.js
@@ -1,45 +1,47 @@
 import Link from "next/link";
 import Image from "next/image";
 import SlickCarousel from "@/app/catalogue/components/SlickCarousel";
+import Slider from "../components/Slider";
 
 export default function Children({ categoryId, parentId, childrenData }) {
-  return (
-    <SlickCarousel>
-      {childrenData.map((child) => {
-        return (
-          child.ParentId === parentId && (
-            <div
-              key={child.id}
-              className="flex flex-col gap-3 items-center justify-between p-5"
-            >
-              <div className="border border-neutral-400 border-b-0 rounded-t-2xl drop-shadow-xl">
-                <div className="relative w-56 h-56 mx-auto">
-                  {child.image ? (
-                    <Image
-                      src={child.image}
-                      srcSet={child.id}
-                      alt={child.id}
-                      fill={true}
-                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                      className="absolute object-contain"
-                      priority={true}
-                    />
-                  ) : (
-                    <div className="w-56 h-56 mx-auto flex flex-col items-center justify-center text-neutral-700 dark:text-neutral-300">
-                      <h1 className="text-xs italic">No Image</h1>
-                    </div>
-                  )}
-                </div>
-              </div>
-              <div className="border border-neutral-400 rounded-b-2xl text-center text-sm py-5 hover:underline underline-offset-2">
-                <Link href={`/catalogue/${categoryId}/${parentId}/${child.id}`}>
-                  <h1>{child.name}</h1>
-                </Link>
-              </div>
-            </div>
-          )
-        );
-      })}
-    </SlickCarousel>
-  );
+	return (
+		// <SlickCarousel>
+		//   {childrenData.map((child) => {
+		//     return (
+		//       child.ParentId === parentId && (
+		//         <div
+		//           key={child.id}
+		//           className="flex flex-col gap-3 items-center justify-between p-5"
+		//         >
+		//           <div className="border border-neutral-400 border-b-0 rounded-t-2xl drop-shadow-xl">
+		//             <div className="relative w-56 h-56 mx-auto">
+		//               {child.image ? (
+		//                 <Image
+		//                   src={child.image}
+		//                   srcSet={child.id}
+		//                   alt={child.id}
+		//                   fill={true}
+		//                   sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+		//                   className="absolute object-contain"
+		//                   priority={true}
+		//                 />
+		//               ) : (
+		//                 <div className="w-56 h-56 mx-auto flex flex-col items-center justify-center text-neutral-700 dark:text-neutral-300">
+		//                   <h1 className="text-xs italic">No Image</h1>
+		//                 </div>
+		//               )}
+		//             </div>
+		//           </div>
+		//           <div className="border border-neutral-400 rounded-b-2xl text-center text-sm py-5 hover:underline underline-offset-2">
+		//             <Link href={`/catalogue/${categoryId}/${parentId}/${child.id}`}>
+		//               <h1>{child.name}</h1>
+		//             </Link>
+		//           </div>
+		//         </div>
+		//       )
+		//     );
+		//   })}
+		// </SlickCarousel>
+		<Slider childrens={childrenData} parentId={parentId} />
+	);
 }
--- src/app/catalogue/components/Slider.jsx
+++ src/app/catalogue/components/Slider.jsx
@@ -1,15 +1,17 @@
 "use client";
 
+import React, { useRef, useCallback } from "react";
+import Link from "next/link";
 import Image from "next/image";
-import React, { useEffect } from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Navigation, Pagination, A11y, Keyboard } from "swiper/modules";
 
+import { motion } from "framer-motion";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "swiper/css/scrollbar";
+import { useIcons } from "@/app/utils/CustomIcons";
 
 const images = [
 	"/images/1.png",
@@ -20,59 +22,210 @@ const images = [
 	"/images/6.png",
 	"/images/7.png",
 ];
-export default function Slider() {
-	// useEffect(() => {
-	// 	const swiper = new Swiper(".swiperContainer", {});
-	// }, []);
+export default function Slider({
+	parents = null,
+	childrens = null,
+	items = null,
+	categoryId = null,
+	parentId = null,
+	childId = null,
+}) {
+	const sliderRef = useRef(null);
 
-	// return (
-	// 	<div className="swiperContainer">
-	// 		<div className="swiperWrapper">
-	// 			{images.map((img, index) => {
-	// 				return (
-	// 					<div key={index} className="relative w-56 h-56 rounded-lg">
-	// 						<Image
-	// 							src={img}
-	// 							alt=""
-	// 							fill
-	// 							className="object-contain object-center"
-	// 						/>
-	// 					</div>
-	// 				);
-	// 			})}
-	// 		</div>
-	// 	</div>
-	// );
+	const { RightIcon, LeftIcon } = useIcons();
+
+	let content;
+
+	if (parents) {
+		content = parents.map((parent) => {
+			return (
+				parent.CategoryId === categoryId && (
+					<SwiperSlide key={parent.id}>
+						<div
+							key={parent.id}
+							className="flex flex-col items-center justify-between my-12"
+						>
+							<div className="border border-neutral-400 border-b-0 rounded-t-2xl drop-shadow-xl">
+								<div className="relative w-56 h-56 mx-auto">
+									{parent.image ? (
+										<Image
+											src={parent.image}
+											alt={parent.id}
+											fill={true}
+											sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+											className="absolute object-contain"
+											priority={true}
+										/>
+									) : (
+										<div className="w-56 h-56 mx-auto flex flex-col items-center justify-center text-neutral-700 dark:text-neutral-300">
+											<h1 className="text-xs italic">No Image</h1>
+										</div>
+									)}
+								</div>
+							</div>
+							<div className="w-56 border border-neutral-400 rounded-b-2xl text-center text-sm py-5 hover:underline underline-offset-2">
+								<Link
+									key={parent.id}
+									href={`/catalogue/${categoryId}/${parent.id}`}
+								>
+									<h1>{parent.name}</h1>
+								</Link>
+							</div>
+						</div>
+					</SwiperSlide>
+				)
+			);
+		});
+	} else if (childrens) {
+		content = childrens.map((child) => {
+			return (
+				child.ParentId === parentId && (
+					<SwiperSlide key={child.id}>
+						<div
+							key={child.id}
+							className="flex flex-col items-center justify-between my-12"
+						>
+							<div className="border border-neutral-400 border-b-0 rounded-t-2xl drop-shadow-xl">
+								<div className="relative w-56 h-56 mx-auto">
+									{child.image ? (
+										<Image
+											src={child.image}
+											alt={child.id}
+											fill={true}
+											sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+											className="absolute object-contain"
+											priority={true}
+										/>
+									) : (
+										<div className="w-56 h-56 mx-auto flex flex-col items-center justify-center text-neutral-700 dark:text-neutral-300">
+											<h1 className="text-xs italic">No Image</h1>
+										</div>
+									)}
+								</div>
+							</div>
+							<div className="w-56 border border-neutral-400 rounded-b-2xl text-center text-sm py-5 hover:underline underline-offset-2 ">
+								<Link
+									key={child.id}
+									href={`/catalogue/${child.CategoryId}/${child.ParentId}/${child.id}`}
+								>
+									<h1>{child.name}</h1>
+								</Link>
+							</div>
+						</div>
+					</SwiperSlide>
+				)
+			);
+		});
+	} else if (items) {
+		content = items.map((item) => {
+			return (
+				item.ChildId === childId && (
+					<SwiperSlide key={item.id}>
+						<div
+							key={item.id}
+							className="flex flex-col items-center justify-between my-12"
+						>
+							<div className="border border-neutral-400 border-b-0 rounded-t-2xl drop-shadow-xl">
+								<div className="relative w-56 h-56 mx-auto">
+									{items.image ? (
+										<Image
+											src={parent.image}
+											alt={parent.id}
+											fill={true}
+											sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+											className="absolute object-contain"
+											priority={true}
+										/>
+									) : (
+										<div className="w-56 h-56 mx-auto flex flex-col items-center justify-center text-neutral-700 dark:text-neutral-300">
+											<h1 className="text-xs italic">No Image</h1>
+										</div>
+									)}
+								</div>
+							</div>
+							<div className="w-56 border border-neutral-400 rounded-b-2xl text-center text-sm py-5 hover:underline underline-offset-2">
+								<Link
+									key={item.id}
+									href={`/catalogue/${item.CategoryId}/${item.ParentId}/${item.ChildId}/${item.id}`}
+								>
+									<h1>{item.name}</h1>
+								</Link>
+							</div>
+						</div>
+					</SwiperSlide>
+				)
+			);
+		});
+	}
+
+	const handlePrev = useCallback(() => {
+		if (!sliderRef.current) return;
+		sliderRef.current.swiper.slidePrev();
+	}, []);
+
+	const handleNext = useCallback(() => {
+		if (!sliderRef.current) return;
+		sliderRef.current.swiper.slideNext();
+	}, []);
 
 	return (
-		<div className="overflow-hidden w-full">
-			<Swiper
-				modules={[Navigation, Pagination, Scrollbar, A11y]}
-				spaceBetween={50}
-				slidesPerView={3}
-				navigation
-				pagination={{ clickable: true }}
-				scrollbar={{ draggable: true }}
-				onSwiper={(swiper) => console.log(swiper)}
-				onSlideChange={() => console.log("slide change")}
+		<div className="overflow-hidden w-full mx-auto relative">
+			<div className="w-[95%] mx-auto">
+				<Swiper
+					ref={sliderRef}
+					modules={[Navigation, Pagination, A11y, Keyboard]}
+					spaceBetween={50}
+					slidesPerView={3}
+					// navigation={{ nextEl: ".arrowRight", prevEl: ".arrowLeft" }}
+					// navigation
+					pagination={{ dynamicBullets: true, clickable: true }}
+					// onSwiper={(swiper) => console.log(swiper)}
+					// onSlideChange={() => console.log("slide change")}
+					grabCursor={true}
+					keyboard={{
+						enabled: true,
+					}}
+					rewind={true}
+					breakpoints={{
+						1366: {
+							slidesPerView: 4,
+						},
+						1024: {
+							slidesPerView: 3,
+						},
+						768: {
+							slidesPerView: 2,
+						},
+						500: {
+							slidesPerView: 1,
+						},
+						320: {
+							slidesPerView: 1,
+						},
+					}}
+				>
+					{content}
+				</Swiper>
+			</div>
+			<motion.div
+				className={`arrowLeft absolute top-0 bottom-0 -left-1 h-fit my-auto grid place-items-center z-10 cursor-pointer`}
+				style={{ display: "block" }}
+				whileTap={{ scale: 0.6 }}
+				whilehover={{ scale: 1.5 }}
+				exit={{ opacity: 0 }}
+				onClick={handlePrev}
 			>
-				{images.map((img, index) => {
-					return (
-						<React.Fragment key={index}>
-							<SwiperSlide>
-								<div className="relative w-56 h-56 rounded-lg">
-									<Image
-										src={img}
-										alt=""
-										fill
-										className="object-contain object-center"
-									/>
-								</div>
-							</SwiperSlide>
-						</React.Fragment>
-					);
-				})}
-			</Swiper>
+				{LeftIcon}
+			</motion.div>
+			<motion.div
+				className={`arrowRight absolute top-0 bottom-0 -right-1 h-fit my-auto grid place-items-center z-10 cursor-pointer`}
+				style={{ display: "block" }}
+				whileTap={{ scale: 0.6 }}
+				exit={{ opacity: 0 }}
+				onClick={handleNext}
+			>
+				{RightIcon}
+			</motion.div>
 		</div>
 	);
 }
