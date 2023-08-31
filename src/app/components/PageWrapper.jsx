29d71ba6824ba2fcc42e21de8693cf786e9e0b6c
Mem0Real <juniourmimo@gmail.com>
2023-08-31 05:49:17 +0300

    Removed smooth scroll for the rest of the site besides landing page

--- src/app/components/PageWrapper.jsx
+++ src/app/components/PageWrapper.jsx
@@ -5,7 +5,7 @@ import SmoothScroller from "./SmoothScroller";
 export default function PageWrapper({ children }) {
 	return (
 		<>
-			<SmoothScroller />
+			{/* <SmoothScroller /> */}
 			<motion.div
 				className="min-h-screen bg-neutral-100 dark:bg-neutral-800 text-neutral-800 dark:text-neutral-200 pt-12"
 				initial={{ opacity: 0, y: 30 }}
