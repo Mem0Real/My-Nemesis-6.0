864acdb46561e6ea677c96a573dffe8a8ecd11c5
Mem0Real <juniourmimo@gmail.com>
2023-09-02 09:45:53 +0300

    dynamic origin works for mobile as well but custom for md

--- src/app/(landing)/components/TopMask.jsx
+++ src/app/(landing)/components/TopMask.jsx
@@ -39,7 +39,7 @@ export default function TopMask() {
 			className="relative z-10 h-[150vh] overflow-clip mt-6 md:-12 lg:mt-24"
 		>
 			<motion.div
-				className={`window absolute left-0 top-0 grid gap-2 p-3 pt-6 [grid-template-rows:4fr_1fr] origin-[${maskPositionX}_${maskPositionY}] `}
+				className={`window absolute left-0 top-0 grid gap-2 p-3 pt-6 [grid-template-rows:4fr_1fr] origin-[${maskPositionX}_${maskPositionY}] md:origin-[94%_32%] lg:origin-[${maskPositionX}_${maskPositionY}] `}
 				style={{ scale }}
 			>
 				<div
